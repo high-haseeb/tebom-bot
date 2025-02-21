42804e5e9f1eb6006391eca11ba6d82bdd967b11
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-02-21 18:59:53 +0500

    refactored aync getter in i18n server

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,7 +3,8 @@ import { getRequestConfig } from 'next-intl/server';
 import { cookies } from 'next/headers';
 
 export default getRequestConfig(async () => {
-    const locale = await cookies().get("locale")?.value || "en";
+    const cookie = await cookies();
+    const locale = cookie.get("locale")?.value || "tr";
 
     return {
         locale,
