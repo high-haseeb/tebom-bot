e0158b27c73a040c589d51c87ca6be73ebe536ef
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-03-07 08:58:57 +0500

    removed sidemenu

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,33 @@ import SideMenu from "@/components/SideMenu";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sooner";
 import OfferList from "@/components/OfferList";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
 
 const Page = () => {
     return (
-        <div className="w-screen bg-background text-foreground sm:overflow-auto overflow-x-hidden lg:overflow-hidden">
-            <div className="isolate flex h-full w-full items-center justify-start">
-                <div className="pointer-events-auto fixed right-4 top-4 z-[90] flex gap-2">
-                    {/* <LanguageSwitcher /> */}
-                    <ThemeToggle />
-                </div>
-                <SidebarProvider className="flex h-full w-full items-center justify-start gap-10">
-                    <SideMenu />
-                    <SidebarInset>
-                        <header className="z-50 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                            <NavigationBar />
-                        </header>
-                        <div className="flex h-auto flex-col items-center justify-center gap-4 p-4 pt-10 lg:flex-row">
-                            <MainForm />
-                            <TrafficInfoDisplay />
-                            <OfferList />
-                        </div>
-                    </SidebarInset>
-                </SidebarProvider>
+        <div className="w-screen min-h-screen bg-background text-foreground sm:overflow-auto overflow-x-hidden lg:overflow-hidden flex flex-col items-center justify-start gap-10 px-4 py-4">
+
+            <div className="flex w-full pointer-events-auto gap-2 items-center justify-center">
+                <div className="mr-auto flex-grow text-2xl font-bold leading-none">Tebom<br/>.net</div>
+                <ThemeToggle />
             </div>
+
+            <div className="flex flex-col h-max items-center justify-center gap-4 lg:flex-row">
+                <MainForm />
+                <TrafficInfoDisplay />
+                <OfferList />
+            </div>
+
             <Toaster />
+
+            <div className="mt-auto w-full flex items-center justify-between">
+                <div className="text-xs text-gray-600">Copyright (c) 2025 high-house. All Rights Reserved.</div>
+                <Link href="https://github.com/high-haseeb/tebom-bot">
+                    <GithubIcon fill="white" size={15} />
+                </Link>
+            </div>
+
         </div>
     )
 }
